23dcabb8c0827f6df4af8dd353376154e14be43f
Fred Feliciano <fred62279@hotmail.com>
2023-07-07 12:30:32 -0400

    Added Countries Endpoint

---
+++ HotelListing.API/Controllers/CountriesController.cs
@@ -0,0 +1,128 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HotelListing.API.Data;
+
+namespace HotelListing.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountriesController : ControllerBase
+    {
+        // added from vid 23
+
+        // Inject DbContext
+        private readonly HotelListingDbContext _context;
+
+        public CountriesController(HotelListingDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Countries
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Country>>> GetCountries()
+        {
+          if (_context.Countries == null)
+          {
+              return NotFound(); //404
+          }
+            // Go to Db and get all country records
+            var countries = await _context.Countries.ToListAsync();
+            return Ok(countries); //200
+        }
+
+        // GET: api/Countries/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Country>> GetCountry(int id)
+        {
+          if (_context.Countries == null)
+          {
+              return NotFound(); //404
+          }
+            var country = await _context.Countries.FindAsync(id);
+
+            if (country == null)
+            {
+                return NotFound(); //404
+            }
+
+            return Ok(country); //200
+        }
+
+        // PUT: api/Countries/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCountry(int id, Country country)
+        {
+            if (id != country.Id)
+            {
+                return BadRequest("Invalid Record Id"); //400
+            }
+
+            _context.Entry(country).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CountryExists(id))
+                {
+                    return NotFound(); //404
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent(); //204
+        }
+
+        // POST: api/Countries
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Country>> PostCountry(Country country)
+        {
+          if (_context.Countries == null)
+          {
+              return Problem("Entity set 'HotelListingDbContext.Countries'  is null.");
+          }
+            _context.Countries.Add(country);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCountry", new { id = country.Id }, country); // 201
+        }
+
+        // DELETE: api/Countries/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCountry(int id)
+        {
+            if (_context.Countries == null)
+            {
+                return NotFound(); //404
+            }
+            var country = await _context.Countries.FindAsync(id);
+            if (country == null)
+            {
+                return NotFound(); //404
+            }
+
+            _context.Countries.Remove(country);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CountryExists(int id)
+        {
+            return (_context.Countries?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
